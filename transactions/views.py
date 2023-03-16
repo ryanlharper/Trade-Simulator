0ee8123b0eb487f81fecd4982fa549634a4885c9
Ryan Harper <ryanlharper@gmail.com>
2023-03-16 11:09:55 -0400

    changes to trade function / still needs work

--- transactions/views.py
+++ transactions/views.py
@@ -1,97 +1,114 @@
 from django.shortcuts import render, redirect
-from django.urls import reverse
 from positions.models import Position
 from paper_trader.forms import TransactionForm
 import yfinance as yf
-from datetime import datetime
-import pandas as pd
-import pandas_market_calendars as mcal
+from datetime import date
 from django.contrib.auth.decorators import login_required
 from django.db.models import Sum
-from decimal import Decimal
-
-# need updates considering accountvalues model addition
+from django.contrib import messages
+from .models import Transaction
+from user_accounts.models import AccountValue, UserAccount
+from django.shortcuts import get_object_or_404
 
 @login_required
-def create_transaction(request):
+def update_position_and_transaction(request):
     if request.method == 'POST':
-        form = TransactionForm(request.POST, initial={'user': request.user})
-        if form.is_valid():
-            transaction = form.save(commit=False)
-            transaction.user = request.user
-            transaction.timestamp = datetime.now()
-            transaction.quantity = form.cleaned_data['quantity']
-            transaction.price = yf.Ticker(transaction.symbol).history(period='1d')['Close'].iloc[-1]
-            
-            # Check if funds are available for the transaction
-            cash_position = Position.objects.filter(user=request.user, symbol='cash').first()  
-            if not cash_position:
-                cash_position = Position(user=request.user, symbol='cash')
-                cash_position.price = 1
-                cash_position.cost = 1
-                cash_position.market_value = 0
-                cash_position.percent_portfolio = 0
-                cash_position.quantity = 0
-                cash_position.save()
-            if transaction.type == 'buy':
-                total_cost = transaction.quantity * transaction.price
-                if cash_position.market_value < total_cost:
-                    return render(request, 'transactions.html', {'form': form})
-                else:
-                    cash_position.market_value -= Decimal(total_cost)
-                    cash_position.save()
-            elif transaction.type == 'sell':
-                position = Position.objects.filter(user=request.user, symbol=transaction.symbol).first()
-                if not position:
-                    form.add_error('symbol', 'Position not found')
-                    return render(request, 'transactions.html', {'form': form})
-                if position.quantity < transaction.quantity:
-                    form.add_error('quantity', 'Not enough shares to sell')
-                    return render(request, 'transactions.html', {'form': form})
-                else:
-                    cash_position.market_value += transaction.quantity * transaction.price
-                    cash_position.save()
-                    if position.quantity == transaction.quantity:  # Full sell
-                        position.delete()
-                    else:  # Partial sell
-                        position.quantity -= transaction.quantity
-                        position.save()
-            nyse = mcal.get_calendar('NYSE')
-            schedule = nyse.schedule(start_date=transaction.timestamp.date(), end_date=transaction.timestamp.date())
+        form = TransactionForm(request.POST)
+        if form.is_valid() and request.user.is_authenticated:
+            transaction_type = form.cleaned_data['type']
+            symbol = form.cleaned_data['symbol']
+            quantity = form.cleaned_data['quantity']
+            notes = form.cleaned_data['notes']
+            user_account = get_object_or_404(UserAccount, user=request.user)
+            user = request.user
+            price = yf.Ticker(symbol).history(period='1d')['Close'].iloc[-1]
+            cost = price
+            market_value = price * quantity
 
-            if len(schedule) == 0:
-                form.add_error(None, 'Market is closed')
-                return render(request, 'transactions.html', {'form': form})
-            else:
-                market_open = pd.Timestamp(schedule.iloc[0]['market_open'].strftime('%Y-%m-%d %H:%M:%S'))
-                market_close = pd.Timestamp(schedule.iloc[0]['market_close'].strftime('%Y-%m-%d %H:%M:%S'))
-                timestamp = pd.Timestamp(transaction.timestamp.strftime('%Y-%m-%d %H:%M:%S'))
-                if timestamp < market_open or timestamp > market_close:
-                    form.add_error(None, 'Market is closed')
-                    return render(request, 'transactions.html', {'form': form})
+            # Get the position for this symbol
+            position, created = Position.objects.get_or_create(user=request.user, symbol=symbol)
 
-            position = Position.objects.filter(user=request.user, symbol=transaction.symbol).first()
-            if not position:
-                position = Position(user=request.user, symbol=transaction.symbol)
-            position.price = transaction.price
-            position.cost = transaction.price
-            position.price_return = (position.price - position.cost) / position.cost * 100
-            if position.quantity is None:
-                position.quantity = transaction.quantity
-            else:
-                position.quantity += transaction.quantity
-            position.market_value = position.price * position.quantity
-            position.percent_portfolio = position.market_value / (Position.objects.filter(user=request.user).aggregate(Sum('market_value'))['market_value__sum'] + cash_position.market_value) * 100
+            # Create or update position
+            if transaction_type == 'buy':
+                if position is None:
+                    position = Position.objects.create(
+                    user=user,
+                    symbol=symbol,                
+                    quantity=quantity,
+                    price=price,
+                    cost = price,
+                    price_return = ((price - cost) / price) * 100,
+                    market_value = market_value,
+                    percent_portoflio = market_value / (Position.objects.filter(user=request.user).aggregate(Sum('market_value'))['market_value__sum']) * 100
+                )
+                    cash_position = Position.objects.get(user=user, symbol='cash')
+                    cash_position.quantity -= price * quantity
+                    cash_position.save()
+                else: 
+                    #figure out averaging cost
+                    #raw SQL "UPDATE positions_position SET quantity = positions.quanity += quantity WHERE symbol = symbol AND user = user"
+                    cash_position = Position.objects.get(user=user, symbol='cash')
+                    cash_position.quantity -= price * quantity
+                    cash_position.save()
+            elif transaction_type == 'sell':
+                if position.quantity < quantity:
+                    messages.error(request, "Not enough shares to sell")
+                    return redirect('update_position_and_transaction')
+                elif position.quantity == quantity:
+                    #delete from positions
+                    position_to_delete = Position.objects.get(user=user, symbol=symbol)
+                    position_to_delete.delete()
+                    # update cash quantity
+                    cash_position = Position.objects.get(user=user, symbol='cash')
+                    cash_position.quantity += price * quantity
+                    cash_position.save()
+                else:
+                    #update position for partial sell
+                    position = Position.objects.create(
+                    user=user,
+                    symbol=symbol,                
+                    quantity=quantity,
+                    price=price,
+                    cost = price,
+                    price_return = ((price - cost) / price) * 100,
+                    market_value = market_value,
+                    percent_portoflio = market_value / (Position.objects.filter(user=request.user).aggregate(Sum('market_value'))['market_value__sum']) * 100
+                )
+                    cash_position = Position.objects.get(user=user, symbol='cash')
+                    cash_position.quantity += price * quantity
+                    cash_position.save()
             position.save()
-            cash_position.market_value -= transaction.price * transaction.quantity if transaction.type == 'buy' else -transaction.price * transaction.quantity
-            cash_position.save()
+
+            # Create a new transaction
+            transaction = Transaction.objects.create(
+                user_account=user_account,
+                user=user,
+                type=transaction_type,
+                symbol=symbol,
+                quantity=quantity,
+                price=price,
+                notes=notes
+            )
             transaction.save()
 
-            return redirect(reverse('transaction_success'))
+            # Update the account value
+            account_value = AccountValue.objects.create(
+                user_account = user_account,
+                date=date.today(),
+                value = sum([p.market_value for p in user.positions.all()]),
+                mtd_return = sum([p.market_value for p in user.positions.all()]), #add last month value to calc
+                ytd_return = sum([p.market_value for p in user.positions.all()])  #add value earliest in current year to calc
+            )
+            account_value.save()
+
+            # Redirect to the success page
+            messages.success(request, "Transaction successful")
+            return redirect('success_page') #this doesn't exisit yet
 
     else:
         form = TransactionForm()
-    return render(request, 'transactions.html', {'form': form, 'title': 'Create Transaction'})
 
-def success_view(request):
-    return render(request, 'transaction_success.html')
+    context = {
+        'form': form
+    }
+    return render(request, 'transactions.html', context)
