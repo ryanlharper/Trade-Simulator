ff393da278faf139c3ccafec83688bcb9e1aa31c
Ryan Harper <ryanlharper@gmail.com>
2023-03-22 10:10:22 -0400

    fix value update

--- user_accounts/utils.py
+++ user_accounts/utils.py
@@ -8,15 +8,5 @@ def update_values():
         value = sum(p.market_value() for p in positions)
         latest_account_value = AccountValue.objects.filter(
             user_account=user_account).order_by('-date').first()
-        # If there is no existing row for the user_account, create a new one
-        if not latest_account_value:
-            account_value = AccountValue(user_account=user_account, date=date.today(), value=value)
-            account_value.save()
-        # If the latest row has a date different than today, create a new one
-        elif latest_account_value.date != date.today():
-            account_value = AccountValue(user_account=user_account, date=date.today(), value=value)
-            account_value.save()
-        # If the latest row already has today's date, update its value
-        else:
-            latest_account_value.value = value
-            latest_account_value.save()
+        latest_account_value.value = value
+        latest_account_value.save()
