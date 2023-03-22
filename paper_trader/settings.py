604f6b71a03582158f2acfee6a5665d96848c3c6
Ryan Harper <ryanlharper@gmail.com>
2023-03-22 10:09:48 -0400

    add css

--- paper_trader/settings.py
+++ paper_trader/settings.py
@@ -129,6 +129,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+else:
+    STATIC_ROOT = [os.path.join(BASE_DIR, "static")]
 
 STATIC_URL = '/static/'
 
