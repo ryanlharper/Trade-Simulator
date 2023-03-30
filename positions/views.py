8013d8554bb12c673a5d99028b2c9ad6a432dc83
Ryan Harper <ryanlharper@gmail.com>
2023-03-30 12:15:36 -0400

    number formatting

--- positions/views.py
+++ positions/views.py
@@ -7,15 +7,35 @@ from .models import User, Position
 def positions_view(request):
     user = request.user
     positions = Position.objects.filter(user=user).order_by('symbol')
-    context = {'positions': positions}
+
+    position_data = []
+    for position in positions:
+        css_class_pct = 'positive' if position.price_return() >= 0 else 'negative_pct'
+        css_class_dollar = 'positive' if position.dollar_return() >= 0 else 'negative_dollar'
+        position_data.append({
+            'position': position,
+            'css_class_pct': css_class_pct,
+            'css_class_dollar': css_class_dollar
+        })
+
+    context = {'position_data': position_data}
     return render(request, 'positions.html', context)
 
 @login_required
 def user_positions(request, user_id):
     user = get_object_or_404(User, pk=user_id)
     positions = Position.objects.filter(user=user).order_by('symbol')
+    
+    position_data = []
+    for position in positions:
+        css_class_dollar = 'positive' if position.dollar_return() >= 0 else 'negative_dollar'
+        position_data.append({
+            'position': position,
+            'css_class_dollar': css_class_dollar
+        })
+    
     context = {
         'user': user,
-        'positions': positions,
+        'position_data': position_data,
     }
     return render(request, 'user_positions.html', context)
