b3a5532828ab0a06dc535087515a449cdab3b647
Ryan Harper <ryanlharper@gmail.com>
2023-03-30 14:27:28 -0400

    style

--- positions/views.py
+++ positions/views.py
@@ -8,17 +8,7 @@ def positions_view(request):
     user = request.user
     positions = Position.objects.filter(user=user).order_by('symbol')
 
-    position_data = []
-    for position in positions:
-        css_class_pct = 'positive' if position.price_return() >= 0 else 'negative_pct'
-        css_class_dollar = 'positive' if position.dollar_return() >= 0 else 'negative_dollar'
-        position_data.append({
-            'position': position,
-            'css_class_pct': css_class_pct,
-            'css_class_dollar': css_class_dollar
-        })
-
-    context = {'position_data': position_data}
+    context = {'positions': positions}
     return render(request, 'positions.html', context)
 
 @login_required
@@ -26,16 +16,8 @@ def user_positions(request, user_id):
     user = get_object_or_404(User, pk=user_id)
     positions = Position.objects.filter(user=user).order_by('symbol')
     
-    position_data = []
-    for position in positions:
-        css_class_dollar = 'positive' if position.dollar_return() >= 0 else 'negative_dollar'
-        position_data.append({
-            'position': position,
-            'css_class_dollar': css_class_dollar
-        })
-    
     context = {
         'user': user,
-        'position_data': position_data,
+        'positions': positions,
     }
     return render(request, 'user_positions.html', context)
